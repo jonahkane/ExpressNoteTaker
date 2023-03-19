2d15dedf293f50372683e9afe157d0ff53f47401
Jonah <jonahtkane@gmail.com>
2023-03-19 18:05:40 -0500

    added code for delete note

--- Develop/db/store.js
+++ Develop/db/store.js
@@ -1,5 +1,6 @@
 const util = require('util')
 const fs = require('fs')
+const uuidv1 = require('uuid/v1')
 
 const readFileAsync = util.promisify(fs.readFile)
 const writeFileAsync = util.promisify(fs.writeFile)
@@ -21,6 +22,17 @@ class Store {
             }
             return parsedNotes
         });
+    }
+    addNote(note) {
+        const {title, text} = note
+        const newNote = {title, text, id: uuidv1()}
+        return this.getNotes().then((notes) => [...notes, newNote]).then((freshNote)=> this.write(freshNote)).then(()=> newNote)
+    }
+    deleteNote(noteId) {
+        console.log(this.getNotes());
+        console.log(noteId);
+        return this.getNotes().then((notes) => notes.filter(note => note.id != noteId)).then((freshNote)=> this.write(freshNote)).then(()=> newNote)
+
     }
 }
 module.exports = new Store()
--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -2,10 +2,20 @@ const router = require("express").Router()
 const store = require('../db/store')
 
 router.get("/notes", (req, res) => {
-    console.log("Hello");
+    console.log("test");
     store.getNotes()
     .then((notes) => {
         return res.json(notes)
     }).catch((err) => res.status(500).json(err))
 })
+router.post("/notes", (req, res) => {
+    store.addNote(req.body)
+    .then((note) => res.json(note)).catch((err) => res.status(500).json(err))    
+})
+router.delete("/notes/:noteId", (req, res) => {
+    store.deleteNote(req.params.noteId)
+    .then((note) => res.json(note)).catch((err) => res.status(500).json(err))     
+})
+// router.put (update) notes.noteId
+
 module.exports = router
